1f5353c6efd17a1f749a2afa9f1980283e543dfa
joeyf <g00361752@gmit.ie>
2021-04-08 15:51:12 +0100

    Transaction test and transaction class added

---
+++ src/main/java/ie/gmit/Transaction.java
@@ -0,0 +1,68 @@
+//Joey Fox
+//G00361752
+//Transaction class
+
+package ie.gmit;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Transaction {
+
+
+    private double amount;
+    private String time;
+    private boolean deposit;
+    private boolean withdraw;
+
+    // public double amount;
+
+    //Transaction constructor
+    public Transaction(double amount, boolean deposit, boolean withdraw){
+
+
+        setTime();
+        setDeposit(deposit);
+        setWithdraw(withdraw);
+        setAmount(amount);
+
+    }
+
+    public boolean isWithdraw() {
+        return withdraw;
+    }
+
+    public void setWithdraw(boolean withdraw) {
+        this.withdraw = withdraw;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public boolean isDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(boolean deposit) {
+        this.deposit = deposit;
+    }
+
+    //puts current time and date into time
+    public void setTime(){
+
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        this.time = (formatter.format(date));
+        //System.out.println(time);
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+
+}
